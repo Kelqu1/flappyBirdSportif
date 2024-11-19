fbba3b90facac529bcf7ff265f4e9bf82ee91814
Kelqu1 <hugolalevee0@gmail.com>
2024-11-19 16:56:11 +0100

    captage des pas

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <Arduino.h>
 
+int nombrePas = 0;
+int lightAncienne = 0;
+
 void setup() {
   pinMode(34, INPUT);
   Serial.begin(9600);
+  Serial.println("démarage");
 }
 
 void loop() {
-  Serial.println(1);
-  int light = analogRead(34);
-  Serial.print("Light = ");
-  Serial.println(light);
+  int lightActuelle = analogRead(34);
+  lightAncienne = lightActuelle;
+  lightActuelle = analogRead(34);
+  if (lightActuelle >0 && lightAncienne == 0) {
+      Serial.println("un est pas détecté");
+      nombrePas++;
+      Serial.print("Nombre de pas: ");
+      Serial.println(nombrePas);
+  }
+  delay(10);  
 }
